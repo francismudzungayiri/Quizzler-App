38342674a532a655347f7513833d6e9d759be62d
francis mudzungayiri <fmudzungayiri@gmail.com>
2021-01-28 23:55:05 +0200

    day 10 learned about classes and objects and an import concept called abstraction

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'quizBrain.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scorekeeper = [];
+  int questionNumber = 0;
+
+  QuizBrain quizBrain = QuizBrain();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.questionBank[questionNumber].questionText,
+                //questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blueGrey,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.blueAccent,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                bool userAnswer = true;
+                if (userAnswer == quizBrain.questionBank[questionNumber].questionAns){
+                  print('user got i right');
+                }else{
+                  print('user got it wrong');
+                }
+                setState(() {
+                  questionNumber= questionNumber+1;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.redAccent,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+               bool userAnswer = false;
+                if (userAnswer == quizBrain.questionBank[questionNumber].questionAns){
+                  print('user got i right');
+                }else{
+                  print('user got it wrong');
+                }
+                setState(() {
+                  questionNumber= questionNumber+1;
+                });
+              },
+            ),
+          ),
+        ),
+        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
---
+++ lib/questions.dart
@@ -0,0 +1,10 @@
+class Question{
+  String questionText;
+  bool questionAns;
+
+  Question(String q, bool a){
+    print('a print  from the constructor ');
+    questionText =q;
+    questionAns = a;
+}
+}
